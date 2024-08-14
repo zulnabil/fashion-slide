7dcbd645109327ccd6fd2325927553848b46e232
Zulnabil Alhadad <zulnabil.faizmoh@efishery.com>
2024-08-14 22:21:39 +0700

    chore(pkg): install sass

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from "path"
+import { fileURLToPath } from "url"
 
-export default nextConfig;
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+}
+
+export default nextConfig
