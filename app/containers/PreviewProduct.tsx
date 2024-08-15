a7476b783c7cfc334234579c73bd26264c1b8087
zulnabil <zulnabil.faizmoh@efishery.com>
2024-08-15 14:59:43 +0700

    feat(slide-preview): finish styling blurry

--- app/containers/PreviewProduct.tsx
+++ app/containers/PreviewProduct.tsx
@@ -1,7 +1,9 @@
 "use client";
 
-import { useProduct } from "../contexts/ProductContext";
-import useGetProductById from "../hooks/useGetProductById";
+import Image from "next/image";
+import { useProduct } from "~/app/contexts/ProductContext";
+import useGetProductById from "~/app/hooks/useGetProductById";
+import "./styles/PreviewProduct.scss";
 
 export default function PreviewProduct() {
   const {
@@ -9,8 +11,17 @@ export default function PreviewProduct() {
   } = useProduct();
   const data = useGetProductById(activeProductId, products[0]);
   return (
-    <div>
+    <div className="preview-product">
+      {/* image as full background backdrop */}
       <h1>{data?.title}</h1>
+      <Image
+        src={data?.images[0]?.url || ""}
+        alt={data?.title || ""}
+        fill
+        objectFit="cover"
+        className="preview-product__backdrop"
+      />
+      <div className="preview-product__blur-box" />
       {/* <Image /> */}
     </div>
   );
